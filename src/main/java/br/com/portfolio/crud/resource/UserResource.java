d10a95a0ac7f5692e2337be80200edfeb9f246c5
lucas-pettinelli <pettinellilucas@gmail.com>
2024-04-25 03:01:07 -0300

    feat: Create 'services' package and 'UserService' class and Implemented the 'findAll' method within the 'UserService' class

--- src/main/java/br/com/portfolio/crud/resource/UserResource.java
+++ src/main/java/br/com/portfolio/crud/resource/UserResource.java
@@ -1,20 +1,36 @@
 package br.com.portfolio.crud.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import br.com.portfolio.crud.entities.User;
+import br.com.portfolio.crud.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
+    @Autowired
+    private UserService service;
 
-        User usuario = new User(1L, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User usuario = service.findById(id);
         return ResponseEntity.ok().body(usuario);
     }
+
 }
